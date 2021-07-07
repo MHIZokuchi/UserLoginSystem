d1d99d6d3b601dabc1df3de3e591ba6a67d28535
Okwuchi Uzoigwe <P7927@fidelitybank.ng>
2021-07-07 15:50:38 +0100

    Added Asp.net Identity

---
+++ MvcProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcProject.Data;
+
+[assembly: HostingStartup(typeof(MvcProject.Areas.Identity.IdentityHostingStartup))]
+namespace MvcProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
